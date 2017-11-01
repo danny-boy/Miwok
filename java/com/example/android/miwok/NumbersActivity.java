d8f83eda8e3d553edcb4555d4ac6d2cd536d434f
daniel <daniel.melchior@gmail.com>
2017-11-01 18:54:42 -0400

    added array

--- java/com/example/android/miwok/NumbersActivity.java
+++ java/com/example/android/miwok/NumbersActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
@@ -11,19 +13,34 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        String[] words = new String[10];
-        words[0] = "one";
-        words[1] = "two";
-        words[2] = "three";
-        words[3] = "four";
-        words[4] = "five";
-        words[5] = "six";
-        words[6] = "seven";
-        words[7] = "eight";
-        words[8] = "nine";
-        words[9] = "ten";
+//        String[] words = new String[10];
+//        words[0] = "one";
+//        words[1] = "two";
+//        words[2] = "three";
+//        words[3] = "four";
+//        words[4] = "five";
+//        words[5] = "six";
+//        words[6] = "seven";
+//        words[7] = "eight";
+//        words[8] = "nine";
+//        words[9] = "ten";
 
-        Log.v("NumbersActivity", "Words at index 0: " + words[0]);
-        Log.v("NumbersActivity", "Words at index 1: " + words[1]);
+        ArrayList<String> words = new ArrayList<>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(0));
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(1));
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(2));
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(3));
     }
 }
