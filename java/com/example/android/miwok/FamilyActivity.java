beb05714190f24911335e70a50199eb084972ead
daniel <daniel.melchior@gmail.com>
2017-11-10 20:48:21 -0500

    added array

--- java/com/example/android/miwok/FamilyActivity.java
+++ java/com/example/android/miwok/FamilyActivity.java
@@ -2,6 +2,8 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
 
@@ -10,4 +12,32 @@ public class FamilyActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_family_members);
     }
+
+    ArrayList<Word> words = new ArrayList<Word>();
+
+//        // Could be created this way...
+//        Word w = new Word("one","lutti");
+//        words.add(w);
+//
+//        // ... But this way is faster
+//
+
+        words.add(new Word("one", "lutti"));
+        words.add(new Word("two", "otiiko"));
+        words.add(new Word("three", "tolookosu"));
+        words.add(new Word("four", "oyyisa"));
+        words.add(new Word("five", "massokka"));
+        words.add(new Word("six", "temmokka"));
+        words.add(new Word("seven", "kenekaku"));
+        words.add(new Word("eight", "kawinta"));
+        words.add(new Word("nine", "wo'e"));
+        words.add(new Word("ten", "na'aacha"));
+
+    WordAdapter adapter = new WordAdapter(this, words);
+
+    ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(adapter);
+
 }
+
